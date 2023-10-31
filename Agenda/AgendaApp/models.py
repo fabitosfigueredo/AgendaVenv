360efb326c892ddddd56d246330fe55e0684ca3e
Fabio Figueredo <fabitofiguereredo@gmail.com>
2023-10-30 22:15:48 -0300

    Alteração de Data Nascimento para Data Nascença

--- Agenda/AgendaApp/models.py
+++ Agenda/AgendaApp/models.py
@@ -17,7 +17,7 @@ class Contato(models.Model):
     nome = models.CharField(max_length=200)
     apelido = models.CharField(max_length=30)
     email = models.EmailField(max_length=100)
-    data_nascimento = models.DateField(verbose_name='Data de Nascimento')
+    data_nascimento = models.DateField(verbose_name='Data de Nascenca')
     endereco = models.CharField(max_length=200, verbose_name='Endereço')
     numero = models.CharField(max_length=10)
     complemento = models.CharField(max_length=50, blank=True, null=True)
