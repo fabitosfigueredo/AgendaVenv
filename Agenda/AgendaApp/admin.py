ce8f6f05938c9d0d3913ff00ae3387b4ef001616
Fabio Figueredo <fabitofiguereredo@gmail.com>
2023-11-06 20:45:06 -0300

    Admin Horizontal Interesse

--- Agenda/AgendaApp/admin.py
+++ Agenda/AgendaApp/admin.py
@@ -11,8 +11,10 @@ class Telefones(admin.TabularInline):
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ['id', 'nome', 'apelido', 'data_nascimento']
     list_filter = ['data_nascimento', 'cidade' ,'estado']
+    list_display_links = ['nome']
     search_fields = ['nome', 'apelido']
     inlines = [Telefones]
+    filter_horizontal = ['interesses']
 
 admin.site.register(Contato, ContatoAdmin)
 admin.site.register(Cidade)
